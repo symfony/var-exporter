a8277637163b02607692b6d9dd2174ffd77130e8
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2025-08-10 00:28:14 +0200

    chore: heredoc indentation as of PHP 7.3
    https://www.php.net/manual/en/language.types.string.php#language.types.string.syntax.heredoc

--- ProxyHelper.php
+++ ProxyHelper.php
@@ -221,20 +221,20 @@ final class ProxyHelper
                 if ('get' === $hook) {
                     $ref = ($method->returnsReference() ? '&' : '');
                     $hooks .= <<<EOPHP
-                            {$ref}get {
-                                return \$this->lazyObjectState->realInstance->{$p->name};
-                            }
+                                {$ref}get {
+                                    return \$this->lazyObjectState->realInstance->{$p->name};
+                                }
 
-                    EOPHP;
+                        EOPHP;
                 } elseif ('set' === $hook) {
                     $parameters = self::exportParameters($method, true);
                     $arg = '$'.$method->getParameters()[0]->name;
                     $hooks .= <<<EOPHP
-                            set({$parameters}) {
-                                \$this->lazyObjectState->realInstance->{$p->name} = {$arg};
-                            }
+                                set({$parameters}) {
+                                    \$this->lazyObjectState->realInstance->{$p->name} = {$arg};
+                                }
 
-                    EOPHP;
+                        EOPHP;
                 } else {
                     throw new LogicException(\sprintf('Cannot generate lazy proxy: hook "%s::%s()" is not supported.', $class->name, $method->name));
                 }
@@ -277,8 +277,8 @@ final class ProxyHelper
                 $body = "        throw new \BadMethodCallException('Cannot forward abstract method \"{$method->class}::{$method->name}()\".');";
             } elseif (str_ends_with($signature, '): never') || str_ends_with($signature, '): void')) {
                 $body = <<<EOPHP
-                        \$this->lazyObjectState->realInstance->{$method->name}({$args});
-                EOPHP;
+                            \$this->lazyObjectState->realInstance->{$method->name}({$args});
+                    EOPHP;
             } else {
                 $mayReturnThis = false;
                 foreach (preg_split('/[()|&]++/', self::exportType($method) ?? 'static') as $type) {
@@ -296,19 +296,19 @@ final class ProxyHelper
 
                 if ($method->returnsReference() || !$mayReturnThis) {
                     $body = <<<EOPHP
-                            return \$this->lazyObjectState->realInstance->{$method->name}({$args});
-                    EOPHP;
+                                return \$this->lazyObjectState->realInstance->{$method->name}({$args});
+                        EOPHP;
                 } else {
                     $body = <<<EOPHP
-                            \${0} = \$this->lazyObjectState->realInstance;
-                            \${1} = \${0}->{$method->name}({$args});
+                                \${0} = \$this->lazyObjectState->realInstance;
+                                \${1} = \${0}->{$method->name}({$args});
 
-                            return match (true) {
-                                \${1} === \${0} => \$this,
-                                !\${1} instanceof \${0} || !\${0} instanceof \${1} => \${1},
-                                null !== \$this->lazyObjectState->cloneInstance =& \${1} => clone \$this,
-                            };
-                    EOPHP;
+                                return match (true) {
+                                    \${1} === \${0} => \$this,
+                                    !\${1} instanceof \${0} || !\${0} instanceof \${1} => \${1},
+                                    null !== \$this->lazyObjectState->cloneInstance =& \${1} => clone \$this,
+                                };
+                        EOPHP;
                 }
             }
             $methods[$lcName] = "    {$signature}\n    {\n{$body}\n    }";
@@ -351,14 +351,14 @@ final class ProxyHelper
         if ($lazyProxyTraitStatement) {
             $lazyProxyTraitStatement = implode("\n        ", $lazyProxyTraitStatement);
             $lazyProxyTraitStatement = <<<EOPHP
-            use \Symfony\Component\VarExporter\Internal\LazyDecoratorTrait {
-                    {$lazyProxyTraitStatement}
-                }
-            EOPHP;
+                use \Symfony\Component\VarExporter\Internal\LazyDecoratorTrait {
+                        {$lazyProxyTraitStatement}
+                    }
+                EOPHP;
         } else {
             $lazyProxyTraitStatement = <<<EOPHP
-            use \Symfony\Component\VarExporter\Internal\LazyDecoratorTrait;
-            EOPHP;
+                use \Symfony\Component\VarExporter\Internal\LazyDecoratorTrait;
+                EOPHP;
         }
 
         return <<<EOPHP
@@ -437,12 +437,12 @@ final class ProxyHelper
                 $body = "        $parentCall;";
             } elseif (str_ends_with($signature, '): never') || str_ends_with($signature, '): void')) {
                 $body = <<<EOPHP
-                        if (isset(\$this->lazyObjectState)) {
-                            (\$this->lazyObjectState->realInstance ??= (\$this->lazyObjectState->initializer)())->{$method->name}({$args});
-                        } else {
-                            {$parentCall};
-                        }
-                EOPHP;
+                            if (isset(\$this->lazyObjectState)) {
+                                (\$this->lazyObjectState->realInstance ??= (\$this->lazyObjectState->initializer)())->{$method->name}({$args});
+                            } else {
+                                {$parentCall};
+                            }
+                    EOPHP;
             } else {
                 if (!$methodsHaveToBeProxied && !$method->isAbstract()) {
                     // Skip proxying methods that might return $this
@@ -459,12 +459,12 @@ final class ProxyHelper
                 }
 
                 $body = <<<EOPHP
-                        if (isset(\$this->lazyObjectState)) {
-                            return (\$this->lazyObjectState->realInstance ??= (\$this->lazyObjectState->initializer)())->{$method->name}({$args});
-                        }
+                            if (isset(\$this->lazyObjectState)) {
+                                return (\$this->lazyObjectState->realInstance ??= (\$this->lazyObjectState->initializer)())->{$method->name}({$args});
+                            }
 
-                        return {$parentCall};
-                EOPHP;
+                            return {$parentCall};
+                    EOPHP;
             }
             $methods[$lcName] = "    {$signature}\n    {\n{$body}\n    }";
         }
@@ -491,10 +491,10 @@ final class ProxyHelper
         ) {
             // fix contravariance type problem when $class declares a `__unserialize()` method without typehint.
             $lazyProxyTraitStatement = <<<EOPHP
-            use \Symfony\Component\VarExporter\LazyProxyTrait {
-                    __unserialize as private __doUnserialize;
-                }
-            EOPHP;
+                use \Symfony\Component\VarExporter\LazyProxyTrait {
+                        __unserialize as private __doUnserialize;
+                    }
+                EOPHP;
 
             $body .= <<<EOPHP
 
@@ -506,8 +506,8 @@ final class ProxyHelper
                 EOPHP;
         } else {
             $lazyProxyTraitStatement = <<<EOPHP
-            use \Symfony\Component\VarExporter\LazyProxyTrait;
-            EOPHP;
+                use \Symfony\Component\VarExporter\LazyProxyTrait;
+                EOPHP;
         }
 
         return <<<EOPHP
--- Tests/LegacyProxyHelperTest.php
+++ Tests/LegacyProxyHelperTest.php
@@ -27,46 +27,46 @@ class LegacyProxyHelperTest extends ProxyHelperTest
     public function testGenerateLazyProxy()
     {
         $expected = <<<'EOPHP'
-         extends \Symfony\Component\VarExporter\Tests\TestForProxyHelper implements \Symfony\Component\VarExporter\LazyObjectInterface
-        {
-            use \Symfony\Component\VarExporter\LazyProxyTrait;
-
-            private const LAZY_OBJECT_PROPERTY_SCOPES = [];
-
-            public function foo1(): ?\Symfony\Component\VarExporter\Tests\Bar
+             extends \Symfony\Component\VarExporter\Tests\TestForProxyHelper implements \Symfony\Component\VarExporter\LazyObjectInterface
             {
-                if (isset($this->lazyObjectState)) {
-                    return ($this->lazyObjectState->realInstance ??= ($this->lazyObjectState->initializer)())->foo1(...\func_get_args());
+                use \Symfony\Component\VarExporter\LazyProxyTrait;
+
+                private const LAZY_OBJECT_PROPERTY_SCOPES = [];
+
+                public function foo1(): ?\Symfony\Component\VarExporter\Tests\Bar
+                {
+                    if (isset($this->lazyObjectState)) {
+                        return ($this->lazyObjectState->realInstance ??= ($this->lazyObjectState->initializer)())->foo1(...\func_get_args());
+                    }
+
+                    return parent::foo1(...\func_get_args());
                 }
 
-                return parent::foo1(...\func_get_args());
-            }
+                public function foo4(\Symfony\Component\VarExporter\Tests\Bar|string $b, &$d): void
+                {
+                    if (isset($this->lazyObjectState)) {
+                        ($this->lazyObjectState->realInstance ??= ($this->lazyObjectState->initializer)())->foo4($b, $d, ...\array_slice(\func_get_args(), 2));
+                    } else {
+                        parent::foo4($b, $d, ...\array_slice(\func_get_args(), 2));
+                    }
+                }
 
-            public function foo4(\Symfony\Component\VarExporter\Tests\Bar|string $b, &$d): void
-            {
-                if (isset($this->lazyObjectState)) {
-                    ($this->lazyObjectState->realInstance ??= ($this->lazyObjectState->initializer)())->foo4($b, $d, ...\array_slice(\func_get_args(), 2));
-                } else {
-                    parent::foo4($b, $d, ...\array_slice(\func_get_args(), 2));
+                protected function foo7()
+                {
+                    if (isset($this->lazyObjectState)) {
+                        return ($this->lazyObjectState->realInstance ??= ($this->lazyObjectState->initializer)())->foo7(...\func_get_args());
+                    }
+
+                    return throw new \BadMethodCallException('Cannot forward abstract method "Symfony\Component\VarExporter\Tests\TestForProxyHelper::foo7()".');
                 }
             }
 
-            protected function foo7()
-            {
-                if (isset($this->lazyObjectState)) {
-                    return ($this->lazyObjectState->realInstance ??= ($this->lazyObjectState->initializer)())->foo7(...\func_get_args());
-                }
+            // Help opcache.preload discover always-needed symbols
+            class_exists(\Symfony\Component\VarExporter\Internal\Hydrator::class);
+            class_exists(\Symfony\Component\VarExporter\Internal\LazyObjectRegistry::class);
+            class_exists(\Symfony\Component\VarExporter\Internal\LazyObjectState::class);
 
-                return throw new \BadMethodCallException('Cannot forward abstract method "Symfony\Component\VarExporter\Tests\TestForProxyHelper::foo7()".');
-            }
-        }
-
-        // Help opcache.preload discover always-needed symbols
-        class_exists(\Symfony\Component\VarExporter\Internal\Hydrator::class);
-        class_exists(\Symfony\Component\VarExporter\Internal\LazyObjectRegistry::class);
-        class_exists(\Symfony\Component\VarExporter\Internal\LazyObjectState::class);
-
-        EOPHP;
+            EOPHP;
 
         $this->assertSame($expected, ProxyHelper::generateLazyProxy(new \ReflectionClass(TestForProxyHelper::class)));
     }
@@ -74,51 +74,51 @@ class LegacyProxyHelperTest extends ProxyHelperTest
     public function testGenerateLazyProxyForInterfaces()
     {
         $expected = <<<'EOPHP'
-         implements \Symfony\Component\VarExporter\Tests\TestForProxyHelperInterface1, \Symfony\Component\VarExporter\Tests\TestForProxyHelperInterface2, \Symfony\Component\VarExporter\LazyObjectInterface
-        {
-            use \Symfony\Component\VarExporter\LazyProxyTrait;
-
-            private const LAZY_OBJECT_PROPERTY_SCOPES = [];
-
-            public function initializeLazyObject(): \Symfony\Component\VarExporter\Tests\TestForProxyHelperInterface1&\Symfony\Component\VarExporter\Tests\TestForProxyHelperInterface2
+             implements \Symfony\Component\VarExporter\Tests\TestForProxyHelperInterface1, \Symfony\Component\VarExporter\Tests\TestForProxyHelperInterface2, \Symfony\Component\VarExporter\LazyObjectInterface
             {
-                if ($state = $this->lazyObjectState ?? null) {
-                    return $state->realInstance ??= ($state->initializer)();
+                use \Symfony\Component\VarExporter\LazyProxyTrait;
+
+                private const LAZY_OBJECT_PROPERTY_SCOPES = [];
+
+                public function initializeLazyObject(): \Symfony\Component\VarExporter\Tests\TestForProxyHelperInterface1&\Symfony\Component\VarExporter\Tests\TestForProxyHelperInterface2
+                {
+                    if ($state = $this->lazyObjectState ?? null) {
+                        return $state->realInstance ??= ($state->initializer)();
+                    }
+
+                    return $this;
                 }
 
-                return $this;
-            }
+                public function foo1(): ?\Symfony\Component\VarExporter\Tests\Bar
+                {
+                    if (isset($this->lazyObjectState)) {
+                        return ($this->lazyObjectState->realInstance ??= ($this->lazyObjectState->initializer)())->foo1(...\func_get_args());
+                    }
 
-            public function foo1(): ?\Symfony\Component\VarExporter\Tests\Bar
-            {
-                if (isset($this->lazyObjectState)) {
-                    return ($this->lazyObjectState->realInstance ??= ($this->lazyObjectState->initializer)())->foo1(...\func_get_args());
+                    return throw new \BadMethodCallException('Cannot forward abstract method "Symfony\Component\VarExporter\Tests\TestForProxyHelperInterface1::foo1()".');
                 }
 
-                return throw new \BadMethodCallException('Cannot forward abstract method "Symfony\Component\VarExporter\Tests\TestForProxyHelperInterface1::foo1()".');
-            }
+                public function foo2(?\Symfony\Component\VarExporter\Tests\Bar $b, ...$d): \Symfony\Component\VarExporter\Tests\TestForProxyHelperInterface2
+                {
+                    if (isset($this->lazyObjectState)) {
+                        return ($this->lazyObjectState->realInstance ??= ($this->lazyObjectState->initializer)())->foo2(...\func_get_args());
+                    }
 
-            public function foo2(?\Symfony\Component\VarExporter\Tests\Bar $b, ...$d): \Symfony\Component\VarExporter\Tests\TestForProxyHelperInterface2
-            {
-                if (isset($this->lazyObjectState)) {
-                    return ($this->lazyObjectState->realInstance ??= ($this->lazyObjectState->initializer)())->foo2(...\func_get_args());
+                    return throw new \BadMethodCallException('Cannot forward abstract method "Symfony\Component\VarExporter\Tests\TestForProxyHelperInterface2::foo2()".');
                 }
 
-                return throw new \BadMethodCallException('Cannot forward abstract method "Symfony\Component\VarExporter\Tests\TestForProxyHelperInterface2::foo2()".');
+                public static function foo3(): string
+                {
+                    throw new \BadMethodCallException('Cannot forward abstract method "Symfony\Component\VarExporter\Tests\TestForProxyHelperInterface2::foo3()".');
+                }
             }
 
-            public static function foo3(): string
-            {
-                throw new \BadMethodCallException('Cannot forward abstract method "Symfony\Component\VarExporter\Tests\TestForProxyHelperInterface2::foo3()".');
-            }
-        }
+            // Help opcache.preload discover always-needed symbols
+            class_exists(\Symfony\Component\VarExporter\Internal\Hydrator::class);
+            class_exists(\Symfony\Component\VarExporter\Internal\LazyObjectRegistry::class);
+            class_exists(\Symfony\Component\VarExporter\Internal\LazyObjectState::class);
 
-        // Help opcache.preload discover always-needed symbols
-        class_exists(\Symfony\Component\VarExporter\Internal\Hydrator::class);
-        class_exists(\Symfony\Component\VarExporter\Internal\LazyObjectRegistry::class);
-        class_exists(\Symfony\Component\VarExporter\Internal\LazyObjectState::class);
-
-        EOPHP;
+            EOPHP;
 
         $this->assertSame($expected, ProxyHelper::generateLazyProxy(null, [new \ReflectionClass(TestForProxyHelperInterface1::class), new \ReflectionClass(TestForProxyHelperInterface2::class)]));
     }
@@ -130,50 +130,50 @@ class LegacyProxyHelperTest extends ProxyHelperTest
             {
             }
         }, <<<'EOPHP'
-        implements \Symfony\Component\VarExporter\LazyObjectInterface
-        {
-            use \Symfony\Component\VarExporter\LazyProxyTrait {
-                __unserialize as private __doUnserialize;
-            }
-
-            private const LAZY_OBJECT_PROPERTY_SCOPES = [];
-
-            public function __unserialize($data): void
+            implements \Symfony\Component\VarExporter\LazyObjectInterface
             {
-                $this->__doUnserialize($data);
+                use \Symfony\Component\VarExporter\LazyProxyTrait {
+                    __unserialize as private __doUnserialize;
+                }
+
+                private const LAZY_OBJECT_PROPERTY_SCOPES = [];
+
+                public function __unserialize($data): void
+                {
+                    $this->__doUnserialize($data);
+                }
             }
-        }
-        EOPHP];
+            EOPHP];
 
         yield 'type hinted __unserialize method' => [new class {
             public function __unserialize(array $array): void
             {
             }
         }, <<<'EOPHP'
-        implements \Symfony\Component\VarExporter\LazyObjectInterface
-        {
-            use \Symfony\Component\VarExporter\LazyProxyTrait;
+            implements \Symfony\Component\VarExporter\LazyObjectInterface
+            {
+                use \Symfony\Component\VarExporter\LazyProxyTrait;
 
-            private const LAZY_OBJECT_PROPERTY_SCOPES = [];
-        }
-        EOPHP];
+                private const LAZY_OBJECT_PROPERTY_SCOPES = [];
+            }
+            EOPHP];
     }
 
     public function testAttributes()
     {
         $expected = <<<'EOPHP'
 
-            public function foo(#[\SensitiveParameter] $a): int
-            {
-                if (isset($this->lazyObjectState)) {
-                    return ($this->lazyObjectState->realInstance ??= ($this->lazyObjectState->initializer)())->foo(...\func_get_args());
+                public function foo(#[\SensitiveParameter] $a): int
+                {
+                    if (isset($this->lazyObjectState)) {
+                        return ($this->lazyObjectState->realInstance ??= ($this->lazyObjectState->initializer)())->foo(...\func_get_args());
+                    }
+
+                    return parent::foo(...\func_get_args());
                 }
-
-                return parent::foo(...\func_get_args());
             }
-        }
 
-        EOPHP;
+            EOPHP;
 
         $class = new \ReflectionClass(new class {
             #[SomeAttribute]
